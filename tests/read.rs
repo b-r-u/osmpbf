3285b74a48f4c146ba5c5c8732c0575dd137ef75
Johannes Hofmann <mail@b-r-u.org>
2017-11-20 14:51:06 +0100

    Refactor read test

--- tests/read.rs
+++ tests/read.rs
@@ -8,59 +8,63 @@ fn approx_eq(a: f64, b: f64) -> bool {
     (a - b).abs() < 1.0e-6
 }
 
+// Compare the content of a HeaderBlock with known values from the test file.
+fn check_header_block_content(block: &HeaderBlock) {
+    assert!(block.required_features().contains(&String::from("OsmSchema-V0.6")));
+    assert!(block.required_features().contains(&String::from("DenseNodes")));
+    assert_eq!(block.optional_features().len(), 0);
+}
+
+// Compare the content of a PrimitiveBlock with known values from the test file.
+fn check_primitive_block_content(block: &PrimitiveBlock) {
+    let nodes = block.groups().flat_map(|g| g.nodes()).count();
+    assert_eq!(nodes, 0);
+
+    {
+        let dense_nodes: Vec<_> = block.groups().flat_map(|g| g.dense_nodes()).collect();
+        assert_eq!(dense_nodes.len(), 3);
+
+        assert!(approx_eq(dense_nodes[1].lat(), 52.11992359584));
+        assert!(approx_eq(dense_nodes[1].lon(), 11.62564468943));
+
+        assert!(approx_eq(dense_nodes[2].lat(), 52.11989910567));
+        assert!(approx_eq(dense_nodes[2].lon(), 11.63101926915));
+
+        assert_eq!(dense_nodes[0].id, 105);
+        assert_eq!(dense_nodes[1].id, 106);
+        assert_eq!(dense_nodes[2].id, 108);
+
+        assert_eq!(dense_nodes[0].uid, 17);
+        assert_eq!(dense_nodes[1].uid, 17);
+        assert_eq!(dense_nodes[2].uid, 17);
+    }
+
+    {
+        let ways: Vec<_> = block.groups().flat_map(|g| g.ways()).collect();
+        assert_eq!(ways.len(), 1);
+
+        let way_tags = ways[0].tags().collect::<Vec<_>>();
+        assert_eq!(way_tags.len(), 2);
+
+        assert!(way_tags.contains(&("building", "yes")));
+        assert!(way_tags.contains(&("name", "triangle")));
+    }
+}
+
 #[test]
 fn read() {
     let reader = BlobReader::from_path(TEST_FILE_PATH).unwrap();
-
     let blobs = reader.collect::<Result<Vec<_>>>().unwrap();
 
     assert_eq!(blobs.len(), 2);
-
     assert_eq!(blobs[0].get_type(), BlobType::OsmHeader);
     assert_eq!(blobs[1].get_type(), BlobType::OsmData);
 
-    {
-        let header = blobs[0].to_headerblock().unwrap();
-        assert!(header.required_features().contains(&String::from("OsmSchema-V0.6")));
-        assert!(header.required_features().contains(&String::from("DenseNodes")));
-    }
+    let header = blobs[0].to_headerblock().unwrap();
+    check_header_block_content(&header);
 
-    {
-        let primitive_block = blobs[1].to_primitiveblock().unwrap();
-
-        let nodes = primitive_block.groups().flat_map(|g| g.nodes()).count();
-        assert_eq!(nodes, 0);
-
-        {
-            let dense_nodes: Vec<_> = primitive_block.groups().flat_map(|g| g.dense_nodes()).collect();
-            assert_eq!(dense_nodes.len(), 3);
-
-            assert!(approx_eq(dense_nodes[1].lat(), 52.11992359584));
-            assert!(approx_eq(dense_nodes[1].lon(), 11.62564468943));
-
-            assert!(approx_eq(dense_nodes[2].lat(), 52.11989910567));
-            assert!(approx_eq(dense_nodes[2].lon(), 11.63101926915));
-
-            assert_eq!(dense_nodes[0].id, 105);
-            assert_eq!(dense_nodes[1].id, 106);
-            assert_eq!(dense_nodes[2].id, 108);
-
-            assert_eq!(dense_nodes[0].uid, 17);
-            assert_eq!(dense_nodes[1].uid, 17);
-            assert_eq!(dense_nodes[2].uid, 17);
-        }
-
-        {
-            let ways: Vec<_> = primitive_block.groups().flat_map(|g| g.ways()).collect();
-            assert_eq!(ways.len(), 1);
-
-            let way_tags = ways[0].tags().collect::<Vec<_>>();
-            assert_eq!(way_tags.len(), 2);
-
-            assert!(way_tags.contains(&("building", "yes")));
-            assert!(way_tags.contains(&("name", "triangle")));
-        }
-    }
+    let primitive_block = blobs[1].to_primitiveblock().unwrap();
+    check_primitive_block_content(&primitive_block);
 }
 
 #[test]
@@ -71,50 +75,17 @@ fn mmap_read() {
     let blobs = reader.collect::<Result<Vec<_>>>().unwrap();
 
     assert_eq!(blobs.len(), 2);
-
     assert_eq!(blobs[0].get_type(), BlobType::OsmHeader);
     assert_eq!(blobs[1].get_type(), BlobType::OsmData);
 
     if let BlobDecode::OsmHeader(header) = blobs[0].decode().unwrap() {
-        assert!(header.required_features().contains(&String::from("OsmSchema-V0.6")));
-        assert!(header.required_features().contains(&String::from("DenseNodes")));
+        check_header_block_content(&header);
     } else {
         panic!("Unexpected blob type");
     }
 
     if let BlobDecode::OsmData(primitive_block) = blobs[1].decode().unwrap() {
-        let nodes = primitive_block.groups().flat_map(|g| g.nodes()).count();
-        assert_eq!(nodes, 0);
-
-        {
-            let dense_nodes: Vec<_> = primitive_block.groups().flat_map(|g| g.dense_nodes()).collect();
-            assert_eq!(dense_nodes.len(), 3);
-
-            assert!(approx_eq(dense_nodes[1].lat(), 52.11992359584));
-            assert!(approx_eq(dense_nodes[1].lon(), 11.62564468943));
-
-            assert!(approx_eq(dense_nodes[2].lat(), 52.11989910567));
-            assert!(approx_eq(dense_nodes[2].lon(), 11.63101926915));
-
-            assert_eq!(dense_nodes[0].id, 105);
-            assert_eq!(dense_nodes[1].id, 106);
-            assert_eq!(dense_nodes[2].id, 108);
-
-            assert_eq!(dense_nodes[0].uid, 17);
-            assert_eq!(dense_nodes[1].uid, 17);
-            assert_eq!(dense_nodes[2].uid, 17);
-        }
-
-        {
-            let ways: Vec<_> = primitive_block.groups().flat_map(|g| g.ways()).collect();
-            assert_eq!(ways.len(), 1);
-
-            let way_tags = ways[0].tags().collect::<Vec<_>>();
-            assert_eq!(way_tags.len(), 2);
-
-            assert!(way_tags.contains(&("building", "yes")));
-            assert!(way_tags.contains(&("name", "triangle")));
-        }
+        check_primitive_block_content(&primitive_block);
     } else {
         panic!("Unexpected blob type");
     }
@@ -129,7 +100,7 @@ fn decode_blob() {
     assert_eq!(blobs[0].get_type(), BlobType::OsmHeader);
     assert_eq!(blobs[1].get_type(), BlobType::OsmData);
 
-    // decoding to the wrong blob type should not panic, but produce an Err.
+    // Decoding to the wrong blob type should not panic but produce an Err.
     assert!(blobs[0].to_primitiveblock().is_err());
     assert!(blobs[1].to_headerblock().is_err());
 
