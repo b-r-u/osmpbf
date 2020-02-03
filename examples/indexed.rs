243e9a9eac02656d0f4e3cabde82272104e166df
Johannes Hofmann <mail@b-r-u.org>
2020-02-03 19:53:42 +0100

    Add IndexedReader example

---
+++ examples/indexed.rs
@@ -0,0 +1,39 @@
+// Count the number of buildings and their nodes from a PBF file
+// given as the first command line argument.
+
+extern crate osmpbf;
+
+use osmpbf::{IndexedReader, Element};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Read command line argument and create IndexedReader
+    let arg = std::env::args_os()
+        .nth(1)
+        .ok_or("need a *.osm.pbf file as argument")?;
+    let mut reader = IndexedReader::from_path(&arg)?;
+
+    println!("Counting...");
+    let mut ways = 0;
+    let mut nodes = 0;
+
+    reader.read_ways_and_deps(
+        |way| {
+            // Filter ways. Return true if tags contain "building": "yes".
+            way.tags().any(|key_value| key_value == ("building", "yes"))
+        },
+        |element| {
+            // Increment counter for ways and nodes
+            match element {
+                Element::Way(_way) => ways += 1,
+                Element::Node(_node) => nodes += 1,
+                Element::DenseNode(_dense_node) => nodes += 1,
+                Element::Relation(_) => {}, // should not occur
+            }
+        },
+    )?;
+
+    // Print result
+    println!("ways:  {}\nnodes: {}", ways, nodes);
+    Ok(())
+}
