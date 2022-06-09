94a0fb5578332c6cdd2ce20f79f0cbd7656d5590
Johannes Hofmann <mail@b-r-u.org>
2022-06-09 21:20:43 +0200

    writer: Add BlockBuilder struct

--- src/writer.rs
+++ src/writer.rs
@@ -1,5 +1,7 @@
 //! Encode and write blobs and blocks
 
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::io::Write;
 
 use byteorder::WriteBytesExt;
@@ -11,7 +13,7 @@ use flate2::{write::ZlibEncoder, Compression};
 use crate::blob::{Blob, BlobType, MAX_BLOB_HEADER_SIZE, MAX_BLOB_MESSAGE_SIZE};
 use crate::block::{HeaderBlock, PrimitiveBlock};
 use crate::error::{new_blob_error, new_protobuf_error, BlobError, Result};
-use crate::proto::fileformat;
+use crate::proto::{fileformat, osmformat};
 
 /// A writer for PBF files that allows writing blobs.
 #[derive(Clone, Debug)]
@@ -123,6 +125,9 @@ impl<W: Write + Send> BlobWriter<W> {
         self.write_blob_raw(header, blob)
     }
 
+    /// Write a new blob that encodes the given [`HeaderBlock`].
+    ///
+    /// The first blob of a `*.osm.pbf` file is usually a header block.
     pub fn write_header_block(&mut self, block: HeaderBlock, encoding: BlobEncoding) -> Result<()> {
         self.write_block_message(
             block.header,
@@ -132,6 +137,9 @@ impl<W: Write + Send> BlobWriter<W> {
         )
     }
 
+    /// Write a new blob that encodes the given [`PrimitiveBlock`].
+    ///
+    /// A primitive block may contain nodes, ways and relations.
     pub fn write_primitive_block(
         &mut self,
         block: PrimitiveBlock,
@@ -148,13 +156,54 @@ impl<W: Write + Send> BlobWriter<W> {
     //TODO write unknown blob type
 }
 
+/// A builder for `PrimitiveBlock`s.
+pub struct BlockBuilder<W: Write + Send> {
+    blob_writer: BlobWriter<W>,
+    block: osmformat::PrimitiveBlock,
+    string_map: HashMap<Vec<u8>, usize>,
+}
+
+impl<W: Write + Send> BlockBuilder<W> {
+    pub fn new(blob_writer: BlobWriter<W>) -> Self {
+        let mut block = osmformat::PrimitiveBlock::new();
+        let mut st = osmformat::StringTable::new();
+        // first element should be blank
+        st.set_s(vec![vec![]].into());
+        block.set_stringtable(st);
+        Self {
+            blob_writer,
+            block,
+            string_map: HashMap::new(),
+        }
+    }
+
+    /// Given a string (`Vec<u8>`) return its index from the string table.
+    /// If the string is not yet included, it will be inserted.
+    pub(crate) fn add_string_table_entry(&mut self, entry: Vec<u8>) -> usize {
+        match self.string_map.entry(entry.clone()) {
+            Entry::Occupied(occ) => *occ.get(),
+            Entry::Vacant(vac) => {
+                let st = self.block.mut_stringtable().mut_s();
+                st.push(entry);
+                *vac.insert(st.len() - 1)
+            }
+        }
+    }
+
+    pub fn finish(mut self, encoding: BlobEncoding) -> Result<BlobWriter<W>> {
+        self.blob_writer
+            .write_primitive_block(PrimitiveBlock::new(self.block), encoding)?;
+        Ok(self.blob_writer)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::proto::osmformat;
 
     #[test]
-    fn test_writer() {
+    fn test_blob_writer() {
         let buf = vec![];
         let mut w = BlobWriter::new(buf);
 
@@ -173,5 +222,17 @@ mod tests {
             w.write_primitive_block(block, BlobEncoding::Zlib { level: 6 })
                 .unwrap();
         }
+    }
+
+    #[test]
+    fn test_block_builder() {
+        let mut buf = vec![];
+        let w = BlobWriter::new(&mut buf);
+        let mut block_builder = BlockBuilder::new(w);
+        assert_eq!(block_builder.add_string_table_entry("abc".into()), 1);
+        assert_eq!(block_builder.add_string_table_entry("xyz".into()), 2);
+        assert_eq!(block_builder.add_string_table_entry("abc".into()), 1);
+        assert_eq!(block_builder.add_string_table_entry("123".into()), 3);
+        block_builder.finish(BlobEncoding::Raw).unwrap();
     }
 }
